1e048a6a16806de1f7a86279603afce3bb847733
Saurabh Potdar <93652523+tce-saurabhp@users.noreply.github.com>
2024-06-07 11:15:04 +0530

    replaced deprecated ClickHouseContainer with GenericContainer

--- src/test/java/com/tce/clickhouse/config/TestContainersInitializer.java
+++ src/test/java/com/tce/clickhouse/config/TestContainersInitializer.java
@@ -3,25 +3,26 @@ package com.tce.clickhouse.config;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.testcontainers.containers.ClickHouseContainer;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
 public class TestContainersInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
-    private static final ClickHouseContainer CLICKHOUSE_CONTAINER;
+    private static final GenericContainer<?> CLICKHOUSE_CONTAINER;
 
     static {
-        DockerImageName imageName = DockerImageName.parse("clickhouse/clickhouse-server:latest");
-        CLICKHOUSE_CONTAINER = new ClickHouseContainer(imageName);
+        final DockerImageName imageName = DockerImageName.parse("clickhouse/clickhouse-server:latest");
+        CLICKHOUSE_CONTAINER = new GenericContainer<>(imageName);
+        CLICKHOUSE_CONTAINER.withExposedPorts(8123);
         CLICKHOUSE_CONTAINER.start();
     }
 
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
         TestPropertyValues values = TestPropertyValues.of(
-                "spring.datasource.url=" + CLICKHOUSE_CONTAINER.getJdbcUrl(),
-                "spring.datasource.username=" + CLICKHOUSE_CONTAINER.getUsername(),
-                "spring.datasource.password=" + CLICKHOUSE_CONTAINER.getPassword()
+                "spring.datasource.url=" + "jdbc:clickhouse://" + CLICKHOUSE_CONTAINER.getHost() + ":" + CLICKHOUSE_CONTAINER.getMappedPort(8123),
+                "spring.datasource.username=" + "default",
+                "spring.datasource.password="
         );
         values.applyTo(applicationContext);
     }
